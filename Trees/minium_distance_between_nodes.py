2185e9fd1860b3ad4738dcd102f66a467baa72a4
alvannwanorim <alvannwanorim@gmail.com>
2024-10-04 08:59:46 +0100

    Feat: new updates

--- Trees/minium_distance_between_nodes.py
+++ Trees/minium_distance_between_nodes.py
@@ -21,3 +21,21 @@ class Solution:
             dfs(root.right)
         dfs(root)
         return res
+
+class Solution2:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        res= []
+        min_val = float('inf')
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        l, r = 0, 1
+        while r < len(res):
+            min_val = min(min_val, res[r] - res[l])
+            l += 1
+            r += 1
+        return min_val
