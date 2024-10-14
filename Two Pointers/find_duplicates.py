4a0aa89407f91a1b1b7c2af3febee49d6d2c5ee6
alvannwanorim <alvannwanorim@gmail.com>
2024-10-14 09:11:21 -0500

    Feat: new updates

--- Two Pointers/find_duplicates.py
+++ Two Pointers/find_duplicates.py
@@ -19,12 +19,13 @@ def elem(nums: List[int], n, e):
 
 def removeDuplicates2(nums: List[int]):
     nums.sort()
-    j = 0
+    j = 1
     for i in range(1,len(nums)):
         if nums[i] != nums[i - 1]:
             nums[j] =nums[i]
             j += 1
     return j
 
-nums = [1,2,3,2,3,4,4,5,5,6]
+# nums = [1,1,2]
+nums = [0,0,1,1,1,2,2,3,3,4]
 print(removeDuplicates2(nums))
